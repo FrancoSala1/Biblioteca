b57c56a7dd9174e3751da05cae35dc99b29c1d0f
Franco Salazar <f.antoniosala@gmai.com>
2024-10-06 23:22:01 -0300

    actualizacion6

---
+++ Clases/paises.py
@@ -0,0 +1,5 @@
+class paises():
+    def __init__(self, codigo_pais, nombre_pais, gentilicio_pais):
+        self.codigo_pais = codigo_pais
+        self.nombre_pais = nombre_pais
+        self.gentilicio_pais = gentilicio_pais
