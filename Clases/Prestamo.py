3f66941647fd09aa617df73914ee9f6514577ea5
Franco Salazar <f.antoniosala@gmai.com>
2024-10-06 21:22:59 -0300

    actualizacion5.2

--- Clases/Prestamo.py
+++ Clases/Prestamo.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
+from Auxiliares import constantes
 import Clases.detalle_libro as detalle_libro
 import Usuario 
+
 class prestamo:
     def __init__(self, id_prestamo, isbn, id_usuario, fecha_prestamo, fecha_devolucion, fecha_devuelto, cantidad_solicitada):
         detalle_libro.__init__(isbn)
